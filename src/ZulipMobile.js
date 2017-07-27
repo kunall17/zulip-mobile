26404fbfbaf5c2e183a4640d24c7161d38accf71
kunall17 <kunall.gupta17@gmail.com>
2017-07-27 20:57:38 +0530

    Add some custom scripts to not build sentry for simulator

--- src/ZulipMobile.js
+++ src/ZulipMobile.js
@@ -1,7 +1,6 @@
 /* @flow */
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { Sentry } from 'react-native-sentry';
 
 import '../vendor/intl/intl';
 import store, { restore } from './store';
@@ -9,10 +8,10 @@ import Providers from './Providers';
 import config from './config';
 
 require('./i18n/locale');
-
-if (config.enableSentry) {
-  Sentry.config(config.sentryKey).install();
-}
+//
+// if (config.enableSentry) {
+//   Sentry.config(config.sentryKey).install();
+// }
 
 export default class ZulipMobile extends Component {
   componentWillMount() {
